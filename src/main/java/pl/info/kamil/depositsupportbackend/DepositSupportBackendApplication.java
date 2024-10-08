d428d4c7d587c37157f13ec3d8304e125ff6f92f
bdaf <kamilkorenkiewiczpw@gmail.com>
2024-10-08 20:16:27 +0200

    Init project with one hello endpoint

---
+++ src/main/java/pl/info/kamil/depositsupportbackend/DepositSupportBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.info.kamil.depositsupportbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepositSupportBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepositSupportBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/info/kamil/depositsupportbackend/DepositSupportBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.info.kamil.depositsupportbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepositSupportBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
